97bab2845e2e372ed5672c66a1962f3041fbb98e
Anmol Singh <604anmolstar@gmail.com>
2024-05-31 01:01:11 +0530

    Install Tailwind and Create Folders

---
+++ src/pages/about/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {FirstLayout} from "../../layouts/FirstLayout";
+
+export default function About() {
+    return (
+        <FirstLayout>
+            <h1>About</h1>
+        </FirstLayout>
+    )
+}
---
+++ src/pages/users/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {FirstLayout} from "../../layouts/FirstLayout";
+
+export default function Users() {
+    return (
+        <FirstLayout>
+            <h1>Users</h1>
+        </FirstLayout>
+    )
+}
