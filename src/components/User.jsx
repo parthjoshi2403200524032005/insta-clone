8936a0c903fa1e12c7ed5b55154e0250e306f5f4
Anmol Singh <604anmolstar@gmail.com>
2024-06-05 10:25:01 +0530

    working

---
+++ src/components/User.jsx
@@ -0,0 +1,10 @@
+// import React from 'react'
+
+// function User() {
+//   alert('hello')
+//   return (<>
+//   </>
+//   )
+// }
+
+// export default User
--- src/pages/Login-Screen/index.jsx
+++ src/pages/Login-Screen/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 // import { SimpleLayout } from '../../layouts/SimpleLayout'
-
+import { Link } from 'react-router-dom'
 function Login() {
   return (
     // <SimpleLayout>
@@ -8,18 +8,17 @@ function Login() {
       <main className='	border-2 rounded p-10  mt-2 bg-white text-black shadow-md	' >
         <div className=''>
           <h1 className='p-8'>Instagram</h1>
-          <input type="text" placeholder='Phone number, username, or email address' className='border-2 rounded p-4 md:p-2 mt-2 bg-white text-black w-full' /><br />
-          <input type="text" placeholder='Password' className='border-2 rounded p-4 md:p-2 mt-2 bg-white text-black w-full' /> <br />
-          <button className='bg-sky-400	 hover:bg-red-400 text-white font-bold py-2 px-4 rounded w-full mt-2'>Log in</button>
+          <input type="text" placeholder='Phone number, username, or email address' className='border-2 rounded p-2 md:p-2 mt-2 bg-white text-black w-full shadow-md' /><br />
+          <input type="password" placeholder='Password' className='border-2 rounded p-2 md:p-2 mt-2 bg-white text-black w-full shadow-md' /> <br />
+          <button  className='bg-sky-400	 hover:bg-red-400 text-white font-bold py-1 px-4 rounded w-full mt-2 '>Log in</button>
           <section className='flex'>
             <div className='w-1/2 m-2 h-0.5 bg-slate-300'></div>
             <div className='ml-0.5'>OR</div>
             <div className='w-1/2 m-2 h-0.5 bg-slate-300'></div>
 
           </section>
-          <div className='mt-2text-blue-500'><a href='https://rb.gy/lblvma'>Log in with Facebook</a></div>
-          <div className='mt-2 text-blue-500'><a href='https://rb.gy/440lob'>Forgetton Your Password? </a></div>
-
+          <div className='mt-2text-black'><Link to="https://bit.ly/3RcM2ve">Log in with Facebook</Link></div>
+          <div className='mt-4 text-gray-400'><Link to='https://bit.ly/4c2r0Hy'>Forgetton Your Password? </Link></div>
         </div>
 
       </main>
