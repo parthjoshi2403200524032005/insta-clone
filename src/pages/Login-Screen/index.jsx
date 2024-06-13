98168fb204782ce74b383e59c82b34fe29bca757
Anmol Singh <604anmolstar@gmail.com>
2024-06-14 03:28:43 +0530

    #Working on layout and signup-login

--- src/pages/Login-Screen/index.jsx
+++ src/pages/Login-Screen/index.jsx
@@ -1,259 +1,90 @@
+import React from "react";
+import { SimpleLayout } from "../../layouts/SimpleLayout";
+import { Link } from "react-router-dom";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+const validationSchema = Yup.object().shape({
+  password: Yup.string().required("Password is required"),
+  email: Yup.string().required("Email is required"),
+});
+function LoginScreen(props) {
+  const loginUserForm = useFormik({
+    initialValues: {
+      password: "",
+      email: "",
+    },
+    onSubmit: (values) => {
+      props.loginUser(values);
+      loginUserForm.resetForm();
+    },
+    validationSchema: validationSchema,
+  });
 
-
-import React from 'react'
-// import { SimpleLayout } from '../../layouts/SimpleLayout'
-import { Link } from 'react-router-dom'
-function Login() {
   return (
-    <div>
-      <main className='	border-2 rounded p-10  mt-2 bg-white text-black shadow-md	' >
-        <div className=''>
-          <h1 className='p-8'>Instagram</h1>
-          <input type="text" placeholder='Phone number, username, or email address' className='border-2 rounded p-2 md:p-2 mt-2 bg-white text-black w-full shadow-md' /><br />
-          <input type="password" placeholder='Password' className='border-2 rounded p-2 md:p-2 mt-2 bg-white text-black w-full shadow-md' /> <br />
-          <button  className='bg-sky-400	 hover:bg-red-400 text-white font-bold py-1 px-4 rounded w-full mt-2 '>Log in</button>
-          <section className='flex'>
-            <div className='w-1/2 m-2 h-0.5 bg-slate-300'></div>
-            <div className='ml-0.5'>OR</div>
-            <div className='w-1/2 m-2 h-0.5 bg-slate-300'></div>
-
+    <SimpleLayout>
+      <form onSubmit={loginUserForm.handleSubmit}>
+        <div className="	border-2 rounded p-10  mt-2 bg-white text-black shadow-md	">
+          <h1 className="p-8">Instagram</h1>
+          <input
+            id="email"
+            name="email"
+            type="text"
+            // placeholder="Email"
+            placeholder="Phone number, username, or email address"
+            className="border-2 rounded p-2 md:p-2 mt-2 bg-white text-black w-full shadow-md text-sm"
+            onChange={loginUserForm.handleChange}
+            onBlur={loginUserForm.handleBlur}
+            value={loginUserForm.values.email}
+          />
+          {loginUserForm.touched.email && loginUserForm.errors.email ? (
+            <div>{loginUserForm.errors.email}</div>
+          ) : null}
+          <input
+            id="password"
+            name="password"
+            type="password"
+            placeholder="Password"
+            className="border-2 rounded p-2 md:p-2 mt-2 bg-white text-black w-full shadow-md text-sm"
+            onChange={loginUserForm.handleChange}
+            onBlur={loginUserForm.handleBlur}
+            value={loginUserForm.values.password}
+          />
+          {loginUserForm.touched.password && loginUserForm.errors.password ? (
+            <div>{loginUserForm.errors.password}</div>
+          ) : null}
+          <button
+            type="submit"
+            className="bg-sky-400	 hover:bg-red-400 text-white font-bold py-1 px-4 rounded-lg w-full my-3 " 
+          >
+            Login
+          </button>
+          <section className="flex">
+            <div className="w-1/2 m-2 h-0.5 bg-slate-300"></div>{" "}
+            <div className="ml-0.5">OR</div>
+            <div className="w-1/2 m-2 h-0.5 bg-slate-300"></div>
           </section>
-          <div className='mt-2text-black'><Link to="https://bit.ly/3RcM2ve">Log in with Facebook</Link></div>
-          <div className='mt-4 text-gray-400'><Link to='https://bit.ly/4c2r0Hy'>Forgetton Your Password? </Link></div>
+          <div className="mt-2text-black">
+            <Link to="https://bit.ly/3RcM2ve">Log in with Facebook</Link>
+          </div>
+          <div className="mt-4 text-gray-400 text-xs ">
+            {" "}
+            <Link to="https://bit.ly/4c2r0Hy" className="text-black">
+              Forgot Password?{" "}
+            </Link>{" "}
+          </div>
         </div>
-
-      </main>
-      <section>
-        <div className='mt-2 border-2 rounded p-4'>Don't have an account? <a href="/signup">Sign up</a></div>
-      </section>
-    </div>
-
-  )
+        <section>
+          <div className="mt-2 border-2 rounded p-4">
+            Don't have an account?{" "}
+            <Link to="/signup" className="text-sky-400">
+              Sign up
+            </Link>
+          </div>
+        </section>
+      </form>
+    </SimpleLayout>
+  );
 }
 
-export default Login
+export default LoginScreen;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import react ,{useState} from 'react';
-// import { useFormik } from 'formik';
-// import * as Yup from 'yup';
-
-// const validationSchema=Yup.object().shape({
-//    id:Yup.String().required("Id is required"),
-//    name:Yup.String().required("Name is required"),
-//    email: Yup.string().matches(/^[\w-\.]+@alexa.com$/, {
-//     message: "Invalid Email",
-//   }),
-//   phone:Yup.String().required("Phone number is required"),
-//   password:Yup.String().required("Password is required"),
-//   confirmPassword: Yup.String().required("Confirm password is required"),
-// });
-// function Login() {
-//   const formik = useFormik({
-//     initialValues: {
-//       id: "",
-//       name: "",
-//       email: "",
-//       phone: "",
-//       password: "",
-//       confirmPassword: "",
-//     },
-//     validationSchema: validationSchema,
-//     onSubmit: (values) => {
-//       console.log(values);
-//     },
-//   });
-//   return (
-//     <div>
-//       <form onSubmit={formik.handleSubmit}>
-//         <div>
-//           <label htmlFor="id">Id</label>
-//           <input
-//             id="id"
-//             name="id"
-//             type="text"
-//             onChange={formik.handleChange}
-//             onBlur={formik.handleBlur}
-//             value={formik.values.id}    
-//           />
-//           {formik.touched.id && formik.errors.id ? (
-//             <div>{formik.errors.id}</div>
-//           ) : null}
-//         </div>
-//         <div>
-//           <label htmlFor="name">Name</label>
-//           <input
-//             id="name"
-//             name="name"
-//             type="text"
-//             onChange={formik.handleChange}
-//             onBlur={formik.handleBlur}
-//             value={formik.values.name}
-//           />
-//           {formik.touched.name && formik.errors.name ? (
-//             <div>{formik.errors.name}</div>
-//           ) : null}
-//         </div>
-//         <div>
-//           <label htmlFor="email">Email</label>
-//           <input
-//             id="email"
-//             name="email"  
-//             type="text"
-//             onChange={formik.handleChange}
-//             onBlur={formik.handleBlur}
-//             value={formik.values.email} 
-//           />
-//           {formik.touched.email && formik.errors.email ? (
-//             <div>{formik.errors.email}</div>
-//           ) : null}
-//         </div>
-//         <div>
-//           <label htmlFor="phone">Phone</label>
-//           <input
-//             id="phone"
-//             name="phone"
-//             type="text"
-//             onChange={formik.handleChange}
-//             onBlur={formik.handleBlur}
-//             value={formik.values.phone}
-//           />
-//           {formik.touched.phone && formik.errors.phone ? (
-//             <div>{formik.errors.phone}</div>
-//           ) : null}
-//         </div>
-//         <div>
-//           <label htmlFor="password">Password</label>
-//           <input
-//             id="password"
-//             name="password"
-//             type="password"
-//             onChange={formik.handleChange}
-//             onBlur={formik.handleBlur}
-//             value={formik.values.password}  
-//           />
-//           {formik.touched.password && formik.errors.password ? (
-//             <div>{formik.errors.password}</div>
-//           ) : null}
-//         </div>
-//         <div>
-//           <label htmlFor="confirmPassword">Confirm Password</label>
-//           <input
-//             id="confirmPassword"
-//             name="confirmPassword"
-//             type="password"
-//             onChange={formik.handleChange}
-//             onBlur={formik.handleBlur}
-//             value={formik.values.confirmPassword}
-//           />
-//           {formik.touched.confirmPassword && formik.errors.confirmPassword ? (
-//             <div>{formik.errors.confirmPassword}</div>
-//           ) : null}
-//         </div>
-//         <button type="submit">Submit</button>
-//       </form>
-//     </div>
-//   );
-// }
-
-// export default Login;
--- src/pages/Signup-Screen/index.jsx
+++ src/pages/Signup-Screen/index.jsx
@@ -1,362 +1,135 @@
-import React from 'react'
-import { SimpleLayout } from '../../layouts/SimpleLayout'
-import { useFormik } from 'formik'
-import * as Yup from 'yup';
+import React from "react";
+import { SimpleLayout } from "../../layouts/SimpleLayout";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { Link } from "react-router-dom";
 const validationSchema = Yup.object().shape({
-  phone: Yup.string().required('Phone number is required'),
+  phone: Yup.string().required("Phone number is required"),
   name: Yup.string().required("Name is required"),
   password: Yup.string().required("Password is required"),
   email: Yup.string().required("Email is required"),
-  // email: Yup.string().matches(/^[\w-\.]+@simbaquartz.com$/, {
-  //   message: "Invalid Email",
-  // }),
-})
+});
 function SignupScreen(props) {
   const signupUserForm = useFormik({
     initialValues: {
-      phone: '',
-      name: '',
-      password: '',
-      email: '',
+      phone: "",
+      name: "",
+      password: "",
+      email: "",
     },
-    validationSchema: validationSchema,
     onSubmit: (values) => {
       props.signupUser(values);
       signupUserForm.resetForm();
-      // console.log(values);
+      
     },
-    validationSchema: validationSchema
-  })
+    validationSchema: validationSchema,
+  });
   return (
     <SimpleLayout>
-      <form onSubmit={signupUserForm.handleSubmit}
-
-        className='border-2 rounded p-10  mt-2 bg-white text-black shadow-md	' htmlFor="phone">
-        <input
-          id="phone"
-          name="phone"
-          type="text"
-          placeholder='Phone number, username, or email address'
-          className='border-2 rounded p-2 md:p-2 mt-2 bg-white text-black w-full shadow-md '
-          onChange={signupUserForm.handleChange}
-          onBlur={signupUserForm.handleBlur}
-          value={signupUserForm.values.phone}
-        />
-        {signupUserForm.touched.phone && signupUserForm.errors.phone ? (
-          <div>{signupUserForm.errors.phone}</div>
-        ) : null}
-        <input
-          id="name"
-          name="name"
-          type="text"
-          placeholder='Full Name'
-          className='border-2 rounded p-2 md:p-2 mt-2 bg-white text-black w-full shadow-md '
-          onChange={signupUserForm.handleChange}
-          onBlur={signupUserForm.handleBlur}
-          value={signupUserForm.values.name}
-        />
-        {signupUserForm.touched.name && signupUserForm.errors.name ? (
-          <div>{signupUserForm.errors.name}</div>
-        ) : null}
-        <input
-          id="email"
-          name="email"
-          type="text"
-          placeholder='Email'
-          className='border-2 rounded p-2 md:p-2 mt-2 bg-white text-black w-full shadow-md '
-          onChange={signupUserForm.handleChange}
-          onBlur={signupUserForm.handleBlur}
-          value={signupUserForm.values.email}
-        />
-        {signupUserForm.touched.email && signupUserForm.errors.email ? (
-          <div>{signupUserForm.errors.email}</div>
-        ) : null} <br />
-        <input
-          id="password"
-          name="password"
-          type="password"
-          placeholder='Password'
-          className='border-2 rounded p-2 md:p-2 mt-2 bg-white text-black w-full shadow-md '
-          onChange={signupUserForm.handleChange}
-          onBlur={signupUserForm.handleBlur}
-          value={signupUserForm.values.password}
-        />
-        <br />
-        {signupUserForm.touched.password && signupUserForm.errors.password ? (
-          <div>{signupUserForm.errors.password}</div>
-        ) : null}
-        <button
-          type="submit"
-          onClick={signupUserForm.handleSubmit}
-          className='bg-sky-400 hover:bg-red-400 text-white font-bold py-1 px-4 rounded w-1/2 mt-2'
-        //  className="bg-blue-500 text-white p-2 rounded-lg"
-        >Signup</button>
+      <form onSubmit={signupUserForm.handleSubmit}>
+        <div className="border-2 rounded p-10  mt-2 bg-white text-black shadow-md">
+          <h1 className="p-8">Instagram</h1>
+          <p className="p-2 font-mono text-gray-500" >
+            Sign up to see photos and videos <br /> from your friends.
+          </p>
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-red-400 text-white font-bold py-1 px-4 rounded-lg w-full my-4"
+          >
+            Log in with Facebook
+          </button>
+          <section className="flex">
+            <div className="w-1/2 m-2 h-0.5 bg-slate-300"></div>{" "}
+            <div className="ml-0.5">OR</div>
+            <div className="w-1/2 m-2 h-0.5 bg-slate-300"></div>
+          </section>
+          <input
+            id="phone"
+            name="phone"
+            type="text"
+            placeholder="Phone number, username, or email address"
+            className="border-2 rounded p-2 md:p-2 mt-2 bg-white text-black w-full shadow-md text-sm "
+            onChange={signupUserForm.handleChange}
+            onBlur={signupUserForm.handleBlur}
+            value={signupUserForm.values.phone}
+          />
+          {signupUserForm.touched.phone && signupUserForm.errors.phone ? (
+            <div>{signupUserForm.errors.phone}</div>
+          ) : null}
+          <input
+            id="name"
+            name="name"
+            type="text"
+            placeholder="Full Name"
+            className="border-2 rounded p-2 md:p-2 mt-2 bg-white text-black w-full shadow-md text-sm  "
+            onChange={signupUserForm.handleChange}
+            onBlur={signupUserForm.handleBlur}
+            value={signupUserForm.values.name}
+          />
+          {signupUserForm.touched.name && signupUserForm.errors.name ? (
+            <div>{signupUserForm.errors.name}</div>
+          ) : null}
+          <input
+            id="email"
+            name="email"
+            type="text"
+            placeholder="Email"
+            className="border-2 rounded p-2 md:p-2 mt-2 bg-white text-black w-full shadow-md text-sm "
+            onChange={signupUserForm.handleChange}
+            onBlur={signupUserForm.handleBlur}
+            value={signupUserForm.values.email}
+          />
+          {signupUserForm.touched.email && signupUserForm.errors.email ? (
+            <div>{signupUserForm.errors.email}</div>
+          ) : null}{" "}
+          <br />
+          <input
+            id="password"
+            name="password"
+            type="password"
+            placeholder="Password"
+            className="border-2 rounded p-2 md:p-2 mt-2 bg-white text-black w-full shadow-md text-sm"
+            onChange={signupUserForm.handleChange}
+            onBlur={signupUserForm.handleBlur}
+            value={signupUserForm.values.password}
+          />
+          <br />
+          {signupUserForm.touched.password && signupUserForm.errors.password ? (
+            <div>{signupUserForm.errors.password}</div>
+          ) : null}
+          <p className="text-gray-500 text-xs">
+            People who use our service may have uploaded <br />
+            your contact information to Instagram.{" "}
+            <span className="text-sky-700">Learn more</span>
+          </p>
+          <p className="text-gray-500 text-xs mt-8">
+            By signing up, you agree to our
+            <span className="text-sky-700">
+              Terms,Privacy <br />
+            </span>
+            Policy and Cookies Policy. <span></span>
+          </p>
+          <button
+            type="submit"
+            onClick={signupUserForm.handleSubmit}
+            className="bg-sky-400 hover:bg-red-400 text-white font-bold py-1 px-4 rounded-lg w-1/2 mt-2"
+            //  className="bg-blue-500 text-white p-2 rounded-lg"
+          >
+            Signup
+          </button>
+        </div>
+        <section>
+          <div className="mt-2 border-2 rounded p-4">
+            Have an account?{" "}
+            <Link to="/login" className="text-sky-400">
+              Log in
+            </Link>
+          </div>
+        </section>
       </form>
     </SimpleLayout>
-  )
+  );
 }
 
-export default SignupScreen
+export default SignupScreen;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-{/* <form>
-      <main className='border-2 rounded p-10  mt-2 bg-white text-black shadow-md	'>
-        <section>
-          <h1>Instagram</h1>
-          <p>Sign up to see photos and videos <br />from your friends.</p>
-          <button className='bg-sky-400 hover:bg-red-400 text-white font-bold py-1 px-4 rounded w-full mt-2'>Log in with Facebook</button>
-
-          <section className='flex'>
-            <div className='w-1/2 m-2 h-0.5 bg-slate-300'></div>
-            <div className='ml-0.5'>OR</div>
-            <div className='w-1/2 m-2 h-0.5 bg-slate-300'></div>
-
-          </section>
-          <input type="text" placeholder='Phone number, username, or email address' className='border-2 rounded p-2 md:p-2 mt-2 bg-white text-black w-full shadow-md ' /> <br />
-          <input type="text" placeholder='Full Name' className='border-2 rounded p-2 md:p-2 mt-2 bg-white text-black w-full shadow-md ' /> <br />
-          <input type="text" placeholder='Username' className='border-2 rounded p-2 md:p-2 mt-2 bg-white text-black w-full shadow-md ' /> <br />
-          <input type="password" placeholder='Password' className='border-2 rounded p-2 md:p-2 mt-2 bg-white text-black w-full shadow-md ' /><br /> <br />
-          <p>People who use our service may have uploaded <br /> your contact information to Instagram. Learn more
-          </p>
-          <br /> <br />
-          <p>By signing up, you agree to our Terms, Privacy <br />Policy and Cookies Policy.</p>
-          <button className='bg-sky-400 hover:bg-red-400 text-white font-bold py-1 px-4 rounded w-full mt-2'>Sign Up</button>
-        </section>
-      </main>
-      <section>
-        <div className='mt-2 border-2 rounded p-4'>Don't have an account? <a href="/signup">Sign up</a></div>
-      </section>
-    </form> */}
